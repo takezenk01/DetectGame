9c68c21bb63d42b8f9cb83e2064c7c3b470e5105
TAKEAKI MAEKAWA <93478155+takezenk01@users.noreply.github.com>
2025-10-29 11:46:42 +0900

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,7 +1,10 @@
+# ===============================================================================
+# YOLO推論用コード
+# ===============================================================================
 import cv2
 from ultralytics import YOLO
 
-# 設定
+# ファイルパス等設定
 WEIGHTS   = r"runs/detect/train/weights/best.pt"  # 学習した重みファイル"
 VIDEO_IN  = r"test_video.mp4"                     # 入力動画パス
 VIDEO_OUT = r"predict.mp4"                        # 出力動画パス
@@ -22,29 +25,39 @@ def decorate_name(label_name, conf):
     """
     ラベル名の最終表記を返す。：
       - 日本語を英語へ（文字化け対策）
-      - 信頼度の追加
+      - 信頼度の取得
     """
     base = RENAME_TABLE.get(label_name, 'unknown')  # テーブルに無ければ元名
     return f"{base} ({conf*100:.0f}%)"             # 例: 「Main_Weapon (87%)」
 
 def main():
+    """
+    入力動画をフレーム単位で読み込み、YOLOで推論した結果を描画して動画を書き出す。
+    - 動画の入出力はを利用
+    - 推論は動画で実行
+    """
+        
     # 動画の取得
     cap = cv2.VideoCapture(VIDEO_IN)
     if not cap.isOpened():
         raise RuntimeError(f"動画が読み込めませんでした。: {VIDEO_IN}")
 
+    # fps, w, h, 動画作成オブジェクト初期化 
     fps   = cap.get(cv2.CAP_PROP_FPS) or 30.0
     w     = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     h     = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     writer = cv2.VideoWriter(VIDEO_OUT, fourcc, fps, (w, h))
 
+    # モデル読み込み
     model = YOLO(WEIGHTS)
 
     # 元クラス名取得
     base_names = model.names if hasattr(model, "names") else {}
 
-    # 推論実行
+    # =========
+    # 推論の実行
+    # =========
     gen = model.predict(
         source=VIDEO_IN,
         device=DEVICE,
@@ -56,10 +69,12 @@ def main():
         verbose=False
     )
 
-    # 1フレームごとに推論結果を描画
+    # ===========================
+    # 推論結果を1フレームごとに描画
+    # ===========================
     frame_idx = 0
     for r in gen:
-        # r.orig_img は BGR の生フレーム
+        # BGRで画像データ受け取り
         frame = r.orig_img.copy()
 
         # 検出されなければ描画しない
@@ -73,6 +88,9 @@ def main():
         confs = r.boxes.conf.cpu().numpy()   # (N,)
         clses = r.boxes.cls.cpu().numpy()    # (N,)
 
+        # ==========================================
+        # 検出ごとにバウンディングボックスとラベルを描画
+        # ==========================================
         for (x1, y1, x2, y2), conf, cls_id in zip(boxes, confs, clses):
             cls_id = int(cls_id)
             orig_name = base_names.get(cls_id, str(cls_id))
@@ -96,6 +114,9 @@ def main():
         writer.write(frame)
         frame_idx += 1
 
+    # ======================
+    # 終了処理（リソース解放）
+    # ======================
     cap.release()
     writer.release()
     print(f"検出結果を反映した動画を作成しました。: {VIDEO_OUT}")
